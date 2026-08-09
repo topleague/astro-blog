f86d34d224775ff809720449d959ad4604b6eec6
topleague <susanta@topleaguetech.com>
2026-08-09 06:21:03 +0530

    three files edited for google font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,20 @@
-// @ts-check
-
+import { defineConfig, fontProviders } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig, fontProviders } from 'astro/config';
 
-// https://astro.build/config
+// https://astro.build
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	site: 'https://suskanchan.pages.dev',
+	integrations: [
+		mdx(), 
+		sitemap()
+	],
 	fonts: [
 		{
-			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
-			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
+			name: 'Playfair Display',
+			cssVariable: '--font-playfair',
+			provider: fontProviders.google(),
+			fallbacks: ['serif'],
 		},
 	],
 });
